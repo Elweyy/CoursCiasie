12cb69bd5138714937751438af71cba6099b9932
Loic <loic.spacher@gmail.com>
2019-03-08 11:58:32 +0100

    .

--- Application_client_web/PROJET-VUE/test.js
+++ Application_client_web/PROJET-VUE/test.js
@@ -15,11 +15,13 @@ Vue.component('artist-list',{
                 <th scope="col"> Name </th>
                 <th scope="col"> Country </th>
                 <th scope="col"> Type </th>
+                <th scope="col"> Albums </th>
             </tr>
             <tr v-for="info in infomusic">
                 <td> {{info.name}} </td>
                 <td> {{info.country}} </td>
                 <td> {{info.type}} </td>
+                <td> <button v-on:click="$emit('showalbumsevent', info.id)"> Afficher les albums </button> </td>
             </tr>
         </table>
         
@@ -39,6 +41,63 @@ Vue.component('music-list',{
             <tr v-for="info in infomusic">
                 <td> {{info.title}} </td>
                 <td> {{info.score}} </td>
+            </tr>
+        </table>
+        
+    `,
+    props:['infomusic']
+})
+
+Vue.component('albums-list',{
+    template:
+    `
+    <table v-if="!infomusic ==''" class="table">
+    <tr>
+        <th scope="col"> Title </th>
+        <th scope="col"> Date </th>
+        <th scope="col"> Releases </th>
+    </tr>
+    <tr v-for="info in infomusic">
+        <td> {{info.title}} </td>
+        <td> {{info.date}} </td>
+        <td>  <button v-on:click="$emit('showtracksevent', info.id)"> Afficher les titres </button> </td>
+    </tr>
+</table>
+
+        
+    `,
+    props:['infomusic']
+})
+
+Vue.component('albums-art-list',{
+    template:
+    `
+        <table v-if="!infomusic ==''" class="table">
+            <tr>
+                <th scope="col"> Title </th>
+                <th scope="col"> Date </th>
+                <th scope="col"> Releases </th>
+            </tr>
+            <tr v-for="info in infomusic">
+                <td> {{info.title}} </td>
+                <td> {{info.date}} </td>
+                <td>  <button v-on:click="$emit('showtracksevent', info.id)"> Afficher les titres </button> </td>
+            </tr>
+        </table>
+        
+    `,
+    props:['infomusic']
+})
+
+Vue.component('albums-tracks-list',{
+    template:
+    `
+        <table v-if="!infomusic ==''" class="table">
+            <tr>
+                <th scope="col"> Title </th>
+            </tr>
+            <tr v-for="info in infomusic">
+                <td> {{info.title}} </td>
             </tr>
         </table>
         
